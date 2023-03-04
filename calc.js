3f953082e4048c0029622df4ae6586b374139ced
JMoss-dev <79190520+JMoss-dev@users.noreply.github.com>
2023-03-04 16:56:38 +0100

    first impl of calculation

--- calc.js
+++ calc.js
@@ -2,18 +2,25 @@ const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
 const output = calculator.querySelector('.calculator_output')
 
+var firstValue = ''
+var op = ''
+var secondValue = ''
+
 keys.addEventListener('click', b => {
     if (b.target.matches('button')) {
         const key = b.target
         const action = key.dataset.action
         const keyContent = key.textContent
-        const outNum = output.textContent;
+        const outNum = output.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
 
-        let array = keys.children
-        //array.forEach(k => {k.classList.remove('isClicked')})
+        Array.from(keys.getElementsByClassName('calculator_key')).forEach(k => {
+            k.classList.remove('isClicked')
+        })
+
 
         if(!action) {
-            if(outNum === '0') {
+            if(outNum === '0' || previousKeyType === 'operator') {
                 output.textContent = keyContent
             }
             else {
@@ -22,8 +29,17 @@ keys.addEventListener('click', b => {
         }
         if (action === 'add' || action === 'sub' ||action === 'mul' ||action === 'div') {
             key.classList.add('isClicked')
+            //add attribute for calculation
+            calculator.dataset.previousKeyType = 'operator'
+
+            firstValue = outNum
+            op = action
         }
-        if (action === 'calc') {}
+        if (action === 'calc') {
+            secondValue = outNum
+
+            output.textContent = calculate()
+        }
         if (action === 'cls') {
             output.textContent = '0'
         }
@@ -33,6 +49,26 @@ keys.addEventListener('click', b => {
     }
 })
 
+//TODO
 keys.addEventListener('hover', h => {
     console.log('hover')
 })
+
+function calculate() {
+    var result = ''
+
+    if (op === 'add') {
+        result = parseFloat(firstValue) + parseFloat(secondValue)
+    }
+    if (op === 'sub') {
+        result = parseFloat(firstValue) - parseFloat(secondValue)
+    }
+    if (op === 'mul') {
+        result = parseFloat(firstValue) * parseFloat(secondValue)
+    }
+    if (op === 'div') {
+        result = parseFloat(firstValue) / parseFloat(secondValue)
+    }
+
+    return result.toFixed(2)
+}
